7321bc1e66ac2a1bce0d7adb4b1dd1989154d183
mochreza1407 <117790296+moch-arjanta@users.noreply.github.com>
2025-08-12 14:02:04 +0700

    update title meta

--- resources/views/layouts/partials/title-meta.blade.php
+++ resources/views/layouts/partials/title-meta.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <title>{{$title}} | IAS Travel </title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+<meta name="title" content="IAS Travel - Admin Dashboard IAS Travel" />
 <meta content="IAS Travel" name="author" />
 
 <!-- App favicon -->
